208e7f4e7806325822fd3ed006efec7c010318b8
mark1346 <markymark331@gmail.com>
2024-09-11 15:58:42 +0900

    init

---
+++ src/test/java/wb/wolbu/WolbuApplicationTests.java
@@ -0,0 +1,13 @@
+package wb.wolbu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WolbuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
